c65b33e12c7ec6a29bc9549440020f95aa82952e
SUNSH1N377 <Sa1mon46@yandex.ru>
2024-04-03 17:03:19 +1000

    обновил код

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,19 @@
-todos = []
+let todos = [];
+
+let id = 0
 
 function addTodo (content) {
-   todos.push(content)
+   let todo = {
+       id: id,
+       content: content
+   }
+   todos.push(todo)
+   id++
 }
+
+// function deleteTodo() {
+//     todos.filter()
+// }
 
 addTodo('Содержимое заметки 1')
 addTodo('Содержимое заметки 2')
